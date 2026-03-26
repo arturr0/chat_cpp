ec6f4ec9668be95e01c491811e0cac3b055920f4
Artur Bednarski <119508496+arturr0@users.noreply.github.com>
2026-03-26 21:43:50 +0100

    Update main.cpp

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,52 +1,119 @@
-#include <uwebsockets/App.h>
-#include <fstream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/filesystem.hpp>
 #include <iostream>
+#include <memory>
 #include <string>
-#include <sstream>
+#include <set>
+#include <thread>
 
-std::string readFile(const std::string& path) {
-    std::ifstream t(path);
-    std::stringstream buffer;
-    buffer << t.rdbuf();
-    return buffer.str();
-}
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace asio = boost::asio;
+using tcp = asio::ip::tcp;
 
-int main() {
-    uWS::App().get("/*", [](auto *res, auto *req) {
-        std::string url = req->getUrl().toString();
-        if (url == "/") url = "/index.html";
+// --- WebSocket session ---
+class WsSession : public std::enable_shared_from_this<WsSession> {
+    websocket::stream<tcp::socket> ws_;
+    std::shared_ptr<std::set<std::shared_ptr<WsSession>>> sessions_;
+    beast::flat_buffer buffer_;
 
-        std::string path = "." + url; // teraz frontend jest w tym samym folderze
-        std::ifstream f(path);
-        if (!f.is_open()) {
-            res->writeStatus("404 Not Found")->end("File not found");
+public:
+    WsSession(tcp::socket socket,
+              std::shared_ptr<std::set<std::shared_ptr<WsSession>>> sessions)
+        : ws_(std::move(socket)), sessions_(sessions) {}
+
+    void start() {
+        ws_.async_accept([self=shared_from_this()](beast::error_code ec){
+            if(!ec){
+                self->sessions_->insert(self);
+                self->do_read();
+            }
+        });
+    }
+
+    void do_read() {
+        ws_.async_read(buffer_, [self=shared_from_this()](beast::error_code ec, std::size_t){
+            if(ec){
+                self->sessions_->erase(self);
+                return;
+            }
+            std::string msg = beast::buffers_to_string(self->buffer_.data());
+
+            // Broadcast to all sessions
+            for(auto& s : *self->sessions_){
+                s->ws_.text(true);
+                s->ws_.async_write(asio::buffer(msg),
+                    [](beast::error_code, std::size_t){});
+            }
+            self->buffer_.consume(self->buffer_.size());
+            self->do_read();
+        });
+    }
+};
+
+// --- HTTP session for static files ---
+void handle_http(tcp::socket socket) {
+    try {
+        beast::flat_buffer buffer;
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req);
+
+        std::string target = req.target().to_string();
+        if(target == "/") target = "/index.html";
+
+        std::string path = "frontend" + target;
+        if(!boost::filesystem::exists(path)){
+            http::response<http::string_body> res{http::status::not_found, 11};
+            res.set(http::field::content_type, "text/plain");
+            res.body() = "Not Found";
+            res.prepare_payload();
+            http::write(socket, res);
             return;
         }
 
-        std::string content = readFile(path);
-        // typ content-type na podstawie rozszerzenia
-        if (url.size() >= 5 && url.substr(url.size()-5) == ".html") res->writeHeader("Content-Type", "text/html");
-        else if (url.size() >= 3 && url.substr(url.size()-3) == ".js") res->writeHeader("Content-Type", "application/javascript");
-        else if (url.size() >= 4 && url.substr(url.size()-4) == ".css") res->writeHeader("Content-Type", "text/css");
-        else res->writeHeader("Content-Type", "text/plain");
+        std::ifstream file(path, std::ios::binary);
+        std::string body((std::istreambuf_iterator<char>(file)),
+                          std::istreambuf_iterator<char>());
 
-        res->end(content);
-    })
-    .ws("/*", {
-        .open = [](auto* ws) {
-            std::cout << "Client connected\n";
-        },
-        .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
-            ws->publish("chat", message, opCode);
-        },
-        .close = [](auto* ws, int code, std::string_view msg) {
-            std::cout << "Client disconnected\n";
+        http::response<http::string_body> res{http::status::ok, 11};
+        if(path.ends_with(".html")) res.set(http::field::content_type, "text/html");
+        else if(path.ends_with(".js")) res.set(http::field::content_type, "application/javascript");
+        else if(path.ends_with(".css")) res.set(http::field::content_type, "text/css");
+        else res.set(http::field::content_type, "text/plain");
+
+        res.body() = body;
+        res.prepare_payload();
+        http::write(socket, res);
+    } catch(...) {}
+}
+
+int main() {
+    try {
+        asio::io_context ioc{1};
+        tcp::acceptor acceptor{ioc, {tcp::v4(), 9001}};
+
+        auto sessions = std::make_shared<std::set<std::shared_ptr<WsSession>>>();
+
+        std::cout << "Server running on port 9001\n";
+
+        while(true){
+            tcp::socket socket{ioc};
+            acceptor.accept(socket);
+
+            // Peek first bytes to see if WebSocket upgrade
+            char data[2];
+            socket.peek(asio::buffer(data,2));
+            if(data[0] == 'G' || data[0]=='P'){ // HTTP GET / upgrade
+                std::thread(handle_http, std::move(socket)).detach();
+            } else {
+                std::make_shared<WsSession>(std::move(socket), sessions)->start();
+            }
         }
-    })
-    .listen(9001, [](auto *token){
-        if (token) {
-            std::cout << "Server running on port 9001\n";
-        }
-    })
-    .run();
+    } catch(std::exception& e){
+        std::cerr << "Error: " << e.what() << "\n";
+    }
 }
