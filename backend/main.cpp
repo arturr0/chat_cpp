ff975cab6986f5b802eeb4ad79c77c978a726289
Artur Bednarski <119508496+arturr0@users.noreply.github.com>
2026-03-26 19:49:36 +0100

    Update main.cpp

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,5 +1,6 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+#include <boost/beast/http.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <iostream>
 #include <thread>
@@ -7,13 +8,35 @@
 
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
+namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
 void handle_client(tcp::socket socket) {
     try {
+        beast::flat_buffer buffer;
+
+        // 👇 Odczyt request HTTP
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req);
+
+        // 👇 Sprawdź czy to WebSocket upgrade
+        if (!websocket::is_upgrade(req)) {
+            std::cout << "Not a WebSocket request\n";
+            return;
+        }
+
         websocket::stream<tcp::socket> ws(std::move(socket));
-        ws.accept();
+
+        // 👇 kluczowe dla Render (proxy compatibility)
+        ws.set_option(websocket::stream_base::timeout::suggested(beast::role_type::server));
+        ws.set_option(websocket::stream_base::decorator(
+            [](websocket::response_type& res) {
+                res.set(http::field::server, "C++ Chat Server");
+            }
+        ));
+
+        ws.accept(req);
 
         std::cout << "Client connected\n";
 
@@ -29,7 +52,7 @@ void handle_client(tcp::socket socket) {
         }
 
     } catch (std::exception const& e) {
-        std::cout << "Client disconnected\n";
+        std::cout << "Client disconnected: " << e.what() << "\n";
     }
 }
 
